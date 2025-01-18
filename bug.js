dff3653e07b8bcbe653135b24417f1cde2da2d7d
BugHunter-X <askzenai@gmail.com>
2025-01-18 20:16:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,10 @@
+```javascript
+// This component uses Tailwind classes but does not import the styles.
+function MyComponent() {
+  return (
+    <div className="bg-blue-500 p-4">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+```
