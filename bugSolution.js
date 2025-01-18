37060b1bcf8bea8084b541df80572885934138bb
BugHunter-X <askzenai@gmail.com>
2025-01-18 20:16:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+import './styles.css'; // Import Tailwind styles
+
+function MyComponent() {
+  return (
+    <div className="bg-blue-500 p-4">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+```
